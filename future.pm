7051865681ed9e80605640dd8ec33152dd4d2fc1
Spencer Tipping <spencer.tipping@gmail.com>
2017-10-24 17:53:59 -0600

    Calculating detailed parse speed

--- future.pm
+++ future.pm
@@ -21,7 +21,17 @@ insertions are efficient. Things like C<seq> are free to (and should) implement
 their own offset arrays to speed up searches.
 
 What's the real change here? Are we just getting rid of start positions and
-canceling reparses if a sub-parser eats the full edit?
+canceling reparses if a sub-parser eats the full edit? ...then start positions
+are dynamically calculated using weak parent links.
+
+Let's also think about immutable object updates: "here's a full parse state,
+take this edit and give me a new one". That makes it possible to confine render
+updates to a single line despite doing more complete parsing.
+
+...after timing, though, render updates aren't what's slow; it's parsing itself
+-- which makes sense. We're parsing at ~2KB/s. This might be ok if we limit the
+scope to a single line at a time and get good at incremental parsing.
+=cut
 
 
 =head1 Parse results
@@ -44,12 +54,14 @@ results provide accessors and could parse on demand.
 3. Inputs should provide a hash(start, length) so we can memoize parses. It's
 expensive to reparse in astable configurations; ideally we'd have an LRU of
 some sort, maybe timed.
+=cut
 
 
 =head1 State management
 Parsers should be immutable objects that we can destructure against and
 reconstruct. Then state modifications amount to building a derivative parser,
 using it, and throwing it away.
+=cut
 
 
 =head1 Operators and inheritance
@@ -66,9 +78,11 @@ One issue here is that we don't really have a way to do polymorphism with this
 strategy. It might not be a problem if we encode an abstract op against a value
 though. Technically that's a more accurate representation: monomorphic ops can
 be inlined, whereas polymorphic ops require a runtime decision.
+=cut
 
 
 =head1 Type propagation
 If we have a list of numbers, say C<[1,2,3]>, we can probably say
 C<[1,2,3] + 1> to distribute. This requires the list to do some type inference
 and runtime-delegate to the items.
+=cut
