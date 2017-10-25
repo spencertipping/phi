6cdf291add3eebae3900ad3d703b175ce6c0b092
Spencer Tipping <spencer.tipping@gmail.com>
2017-10-25 08:06:26 -0600

    Simple calc grammar; the interface is pretty clunky still

--- continuation.pm
+++ continuation.pm
@@ -1,8 +1,11 @@
 =head1 Continuation
-1. Think about parser representation, but don't worry about it too much.
-2. Get a trivial test case for flatmapping parsers.
-3. Start writing some DSLs for common things like data processing or CAD.
-4. Write a very simple shell.
-5. Prototype Perl JIT.
-6. Prototype C JIT.
+1. [done] Think about parser representation, but don't worry about it too much.
+
+2. Refactor parsers to not store start locations, and to support insert/delete
+deltas.
+3. Get a trivial test case for flatmapping parsers.
+4. Start writing some DSLs for common things like data processing or CAD.
+5. Write a very simple shell.
+6. Prototype Perl JIT.
+7. Prototype C JIT.
 =cut
