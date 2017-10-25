f9cded02d1b0b19f5a6606c2ded0a8123f9cdcd8
Spencer Tipping <spencer.tipping@gmail.com>
2017-10-24 22:31:33 -0600

    Saving a continuation

---
+++ continuation.pm
@@ -0,0 +1,6 @@
+=head1 Continuation
+1. Think about parser representation, but don't worry about it too much.
+2. Get a trivial test case for flatmapping parsers.
+3. Start writing some DSLs for common things like data processing or CAD.
+4. Write a very simple shell.
+=cut
