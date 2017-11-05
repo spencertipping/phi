02c0e2fada4441ec1c92687369a427baa8585f02
Spencer Tipping <spencer.tipping@gmail.com>
2017-11-05 13:47:30 -0700

    New continuation

--- continuation.pm
+++ continuation.pm
@@ -1,14 +1,6 @@
-=head1 Precedence and scopes
-Operator precedence isn't parsed by values; values simply take over the parsing
-context to do type-specific things. They can provide ways to access operators
-that aren't precedence-aware. Precedence is implemented by operator-precedence
-scopes.
-
-Scope grammar:
-
-1. C<"(" expr ")">
-2. C(name >cc $scope)
-3. C<alt(@structs)>
-
-Values are always parsed with a type-specific continuation on a loop.
+=head1 Struct representation
+Right now the stuff in phi::struct is a disaster because it's using ad-hoc
+representations of abstracts. If we have an abstract encoding, it should
+correspond to struct schemas somehow -- and we'll need bindings in the language
+to create new structs and parsers.
 =cut
