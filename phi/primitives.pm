9bd1be577613cbb23c018de208f53bc1055a5395
Spencer Tipping <spencer.tipping@gmail.com>
2017-11-05 05:49:27 -0700

    WIP primitive type implementations

---
+++ phi/primitives.pm
@@ -0,0 +1,22 @@
+=head1 Primitive structs
+Base types used to bootstrap the phi language in terms of perl.
+=cut
+
+use strict;
+use warnings;
+
+use phi::syntax;
+use phi::compiler;
+
+
+=head1 Numeric types
+The usual suspects.
+=cut
+
+package phi::struct::int
+{
+  sub parse
+  {
+    
+  }
+}
