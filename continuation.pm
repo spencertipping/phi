793508ce34cbd21750138ef733313ede8aefb7da
Spencer Tipping <spencer.tipping@gmail.com>
2017-11-10 04:51:32 -0700

    Thinking about what we really need to bootstrap stuff

--- continuation.pm
+++ continuation.pm
@@ -4,6 +4,18 @@ of primitives and an encoding of structs-as-values (quoted structs).
 =cut
 
 
+=head1 Primitives
+We need four literal types to bootstrap the compiler:
+
+1. Strings
+2. Ints
+3. Heterogeneous lists
+4. Functions
+
+This gives us enough to describe structs and parsers.
+=cut
+
+
 =head1 Operator precedence and continuations
 The scope can use operator-precedence parsing and still delegate all operators
 to type-specific continuations. Precedence happens leftwards, and the operator
