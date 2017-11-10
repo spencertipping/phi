9269c97497421926d4fed21b1842905fabc7b9a3
Spencer Tipping <spencer.tipping@gmail.com>
2017-11-10 04:46:06 -0700

    WIP: quoted structs first, then flatmap into opaque at the last moment

--- continuation.pm
+++ continuation.pm
@@ -1,8 +1,6 @@
 =head1 Struct representation
-Right now the stuff in phi::struct is a disaster because it's using ad-hoc
-representations of abstracts. If we have an abstract encoding, it should
-correspond to struct schemas somehow -- and we'll need bindings in the language
-to create new structs and parsers.
+We can flatmap structs just like we can flatmap parsers. So all we need is a set
+of primitives and an encoding of structs-as-values (quoted structs).
 =cut
 
 
