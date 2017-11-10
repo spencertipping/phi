574556f5b4744d98a6a9bfae5c7a9f0779f3f93a
Spencer Tipping <spencer.tipping@gmail.com>
2017-11-10 06:25:48 -0700

    WIP primitive/IO

--- continuation.pm
+++ continuation.pm
@@ -22,6 +22,43 @@ has two parts:
 (1) doesn't help much since we can easily create a meaninglessly vague bound,
 but is worth mentioning. (2) is more interesting and deserves some discussion.
 
+There's a small handful of operations that are primitive: they are fully
+monomorphic, untyped, and have no abstraction potential. For instance, objects
+don't get to ponder the question "what type are you"; they store this value
+rather than computing it. This is how the bootstrap layer grounds itself out.
+
+Class objects also have a monomorphic protocol, but it provides a lot of
+flexibility. They need to implement a few operations:
+
+1. parse_continuation($scope)
+2. scope_continuation($scope)
+3. method($self, $name, @args)
+
+IO is maintained within dynamic scopes, so scope_continuation() provides a way
+for values to interact with IO. method() is a universal protocol that makes it
+possible to address values whose type is so vague that no parse continuation is
+useful -- it also handles operators that are parsed by the scope.
+
+=head2 Working with IO
+Abstract IO is a journal of updates encoded into a timeline. In list terms it
+goes backwards; future events are consed onto the existing list. Abstract values
+hold references to various points along the IO timeline to indicate dependency
+ordering.
+
+Compilation happens solely against this IO timeline: the very last value is
+considered to be "returned" from the program, just like in Haskell.
+
+=head2 Errors and the IO monad
+The semicolon operator is a monadic transform (a flatmap, I think). Some
+operations like checked downcasts or OOB tuple accesses will fail, however:
+their values are undefined and erroneous.
+
+Erroneous values aren't themselves problematic: you can store an error into a
+data structure. The problem comes in when the IO monad itself holds an erroneous
+state. This causes the program to fail unless you have a failover strategy
+(try/catch). In more familiar terms, the IO continuation is parsed rather than
+directly computed.
+
 =head2 Primitives
 We need four literal types to bootstrap the compiler:
 
