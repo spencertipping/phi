1971b0e10574ede52c42b4d8b48c92b1252ea98d
Spencer Tipping <spencer.tipping@gmail.com>
2017-11-19 21:54:39 -0700

    Addressed a bunch of continuation entries

--- continuation.pm
+++ continuation.pm
@@ -1,27 +1,6 @@
 =head1 Notes about the bootstrap compiler
 Getting this thing designed and shipped.
 
-=head2 Abstract evaluation
-Abstract evaluation is real evaluation, just with quoted semantics. We can
-resolve certain aspects of structs at abstract-time, erasing them from the
-runtime.
-
-The hard part of abstract evaluation comes in when we need stuff like flatmapped
-parsers. This forces our evaluation model to be value-polymorphic because we're
-using "real" values inside a world of quoted abstracts when we ask for things
-like parse continuations. For example, do those real values get an IO?
-
-=head2 Abstract recursion
-For example:
-
-  f = |i:int| i.gt(0).if(|| i.times(f(i.dec)), || 1)
-
-Expansion never happens inline. Instead, optimization parsers match
-polymorphically against various levels of flattening and force lazy values.
-
-This means we have an op for function calls, and that edge can project downwards
-into its constituent parts.
-
 =head2 Working with IO
 Abstract IO is a journal of updates encoded into a timeline. In list terms it
 goes backwards; future events are consed onto the existing list. Abstract values
@@ -56,16 +35,4 @@ data structure. The problem comes in when the IO monad itself holds an erroneous
 state. This causes the program to fail unless you have a failover strategy
 (try/catch). In more familiar terms, the IO continuation is parsed rather than
 directly computed.
-
-=head2 Primitives
-We need four literal types to bootstrap the compiler:
-
-1. Strings
-2. Ints
-3. Struct literals
-4. Functions
-
-This gives us enough to describe structs and parsers.
-
-We need both consts and abstracts for each of these types.
 =cut
