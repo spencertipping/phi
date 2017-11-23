84f6206b9679153424de68a9069fbc42fadea3c7
Spencer Tipping <spencer.tipping@gmail.com>
2017-11-22 17:36:55 -0700

    More design

--- bytecode.pm
+++ bytecode.pm
@@ -10,8 +10,8 @@ parser bytecodes that form the execution layer. For example:
   00000000  xxxxxxxx yyyyyyyy : function call
   00000001  xxxxxxxx xxxxxxxx : int64 constant
   00000002  llllllll ssss...  : string constant
-
-  00000003..ffffffff xxxxxxxx : method
+  00000003..7fffffff xxxxxxxx : method
+  80000000..ffffffff ...      : optimized encodings
 
 We can write actual code if we have a boot parser that contains enough rewrite
 bindings to be useful. I think it can all be specified in terms of rewriting
@@ -25,4 +25,18 @@ builtin methods.
 Q: does it make sense to get operator precedence/etc sorted out before we do
 this?
 
-Q: does it make sense to continue prototyping in perl?
+Q: does it make sense to continue prototyping in perl? (Yep; if we use C or
+something, it becomes a lot harder to do the quick introspection stuff that
+makes this language easy to work with.)
+
+=head2 Parsing using builtins
+What's involved really? We need string access, integers, and tolerance for a lot
+of memory allocation for return values. The latter part might require a simple
+GC; Boehm should do it.
+
+Is it possible to use perl to compile to initial C bytecode, or even to manage
+the C runtime? The former, sure; we need only minimal parse continuations to
+make it work.
+
+JIT the parsers? Does it make sense to have a pre-compile stage that generates
+precompiled rewriting rules for the program?
