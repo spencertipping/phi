54b34a27cdd0976b6614c1a2b7169f91a1a62c02
Spencer Tipping <spencer.tipping@gmail.com>
2017-11-24 09:52:20 -0700

    WIP figuring out how this language works internally

--- phi.ml
+++ phi.ml
@@ -7,9 +7,7 @@ module rec PhiVal : sig
   | Method of int * t
   | Call   of t * t
 
-  val str_symbol : string -> t
-  val sym_method : t -> t -> t
-  val str_method : string -> t -> t
+  (* TODO: entry points *)
 end = struct
   type t =
   | Nil
@@ -22,12 +20,24 @@ end = struct
   exception PhiNotASymbolExn of PhiVal.t
 
   let str_symbol s = Symbol (Hashtbl.hash s, s)
+  let str_method s v = Method (Hashtbl.hash s, v)
   let sym_method m v =
     match m with
     | Symbol (mh, _) -> Method (mh, v)
     | _              -> raise (PhiNotASymbolExn m)
 
-  let str_method s v = Method (Hashtbl.hash s, v)
+  let nil_type    = Int 0
+  let int_type    = Int 1
+  let string_type = Int 2
+  let symbol_type = Int 3
+  let method_type = Int 4
+  let call_type   = Int 5
+
+  let val_type v = match v with
+  | Nil      -> nil_type
+  | Int _    -> int_type
+  | String _ -> string_type
+  | Symbol _ -> symbol_type
+  | Method _ -> method_type
+  | Call _   -> call_type
 end
-
-
