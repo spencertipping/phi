365b7899c106ce76bb04b817ffffb762873da92e
Spencer Tipping <spencer.tipping@gmail.com>
2017-11-27 07:45:52 -0700

    WIP ocaml implementation

--- phi.ml
+++ phi.ml
@@ -18,32 +18,65 @@ end = struct
   | Call   of t * t
 end
 
+module ParserCombinators = struct
+  open PhiVal
+
+  (* String parsers *)
+  let str s (v, i) =
+    if i + (String.length s) < String.length v
+    && s = String.sub v i (String.length s) then Some (s, (v, i + String.length s))
+    else                                         None
+
+  let chartest b cs (v, i) =
+    if i < String.length v && b = String.contains cs v.[i] then
+      Some (v.[i], (v, i + 1))
+    else None
+
+  let oneof  = chartest true
+  let noneof = chartest false
+
+  (* phi value (structural) parsers *)
+  (* ??? how to do continuations here? *)
+  let emit (v, k) = Some (v, k)
+
+  let empty i = Some ((), i)
+
+  let seq f g i = match f i with
+    | None          -> None
+    | Some (r1, i') -> match g i' with
+      | None           -> None
+      | Some (r2, i'') -> Some ((r1, r2), i'')
+  let (++) = seq
+
+  let either f g i = match f i with
+    | None -> g i
+    | x    -> x
+
+  let rec any xs i = match xs with
+    | []       -> None
+    | x :: xs' -> match x i with
+      | None -> any xs' i
+      | r    -> r
+
+  let rep p min i =
+    let rec rep' min i rs =
+      match p i with
+        | Some (r, i') -> rep' (min-1) i' (r :: rs)
+        | None         -> if min <= 0 then Some (List.rev rs, i)
+                          else             None in
+    rep' min i []
+
+  let star p = rep p 0
+  let plus p = rep p 1
+
+  let p_mbind p f i = match p i with
+    | Some (v, i') -> f v i'
+    | None         -> None
+
+  let p_map p f i = match p i with
+    | Some (v, i') -> Some (f v, i')
+    | None         -> None
+end
+
 open PhiVal
-
-exception PhiNotASymbolExn of PhiVal.t
-
-let str_symbol s   = Symbol (Hashtbl.hash s, s)
-let str_method s v = Method (Hashtbl.hash s, v)
-let sym_method m v =
-  match m with
-  | Symbol (mh, _) -> Method (mh, v)
-  | _              -> raise (PhiNotASymbolExn m)
-
-let nil_type    = Int 0
-let int_type    = Int 1
-let string_type = Int 2
-let symbol_type = Int 3
-let cons_type   = Int 4
-let method_type = Int 5
-let call_type   = Int 6
-
-let val_type v = match v with
-| Nil      -> nil_type
-| Int _    -> int_type
-| String _ -> string_type
-| Symbol _ -> symbol_type
-| Cons _   -> cons_type
-| Method _ -> method_type
-| Call _   -> call_type ;;
-
-print_string "hi!\n"
+open ParserCombinators
