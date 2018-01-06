76ae4ad0f14b1609867fe1f9725fcdc1f32d9f49
Spencer Tipping <spencer.tipping@gmail.com>
2018-01-06 05:37:51 -0700

    Abbreviated struct names, more interpreter stuff

--- interpreter.ml
+++ interpreter.ml
@@ -1,19 +1,23 @@
 module rec PhiVal : sig
   type t =
-  | Int     of int
-  | String  of bytes
-  | Symbol  of int * string
-  | Mutable of t option ref
-  | Nil
-  | Cons    of t * t
+  | I of int
+  | S of bytes
+  | Y of int * string
+  | M of t option ref
+  | N
+  | C of t * t
+
+  type i = P of t * t * t
 end = struct
   type t =
-  | Int     of int
-  | String  of bytes
-  | Symbol  of int * string
-  | Mutable of t option ref
-  | Nil
-  | Cons    of t * t
+  | I of int
+  | S of bytes
+  | Y of int * string
+  | M of t option ref
+  | N
+  | C of t * t
+
+  type i = P of t * t * t
 end
 
 module PhiInterpreter = struct
@@ -21,24 +25,62 @@ module PhiInterpreter = struct
 
   exception OhComeOn of PhiVal.t
 
-  let mksym s  = Symbol (Hashtbl.hash s, s)
-  let mkmut () = Mutable (ref None)
-  let mkstr s  = String (Bytes.of_string s)
+  let mksym s  = Y (Hashtbl.hash s, s)
+  let mkmut () = M (ref None)
+  let mkstr s  = S (Bytes.of_string s)
 
-  let interp d c r = Cons (d, Cons (c, Cons (r, Nil)))
+  let rec resolve v = match v with
+    | M (Some x) -> resolve x
+    | x          -> x
+
+  let interp d c r = P(resolve d, resolve c, resolve r)
 
   let rewrite_fn f = function
-    | Cons (d, Cons (c, Cons (r, Nil))) -> f c d r
-    | x                                 -> raise (OhComeOn x)
+    | P(d, c, r) -> f (resolve d) (resolve c) (resolve r)
+    | x          -> raise (OhComeOn x)
 
-  let iquote   d c r = interp (Cons ((interp d c r), d)) c r
-  let iunquote d c r = interp d c r
-  let ieval    d c r = match d with
-    | Cons (x, d') -> interp d' (Cons (x, c)) r
-    | x            -> raise (OhComeOn x)
+  let iquote   d c r = interp (C(C(d, C(c, C(r, N))), d)) c r
+  let iunquote d c r = match d with
+    | C(d', C(c', C(r', N))) -> interp d' c' r'
+    | x                      -> raise (OhComeOn x)
+
+  let ieval d c r = match d with
+    | C(x, d') -> interp d' (C(x, c)) r
+    | x        -> raise (OhComeOn x)
 
   let vfn f d c r = interp (f d) c r
   let vfn1 f d c r = match d with
-    | Cons (x, d') -> f x d'
-    | x            -> raise (OhComeOn x)
+    | C(x, d') -> f (resolve x) d'
+    | x        -> raise (OhComeOn x)
+
+  let vfn2 f d c r = match d with
+    | C(x, C(y, d')) -> f (resolve x) (resolve y) d'
+    | x              -> raise (OhComeOn x)
+
+  let intsym  = mksym "int"
+  let strsym  = mksym "string"
+  let symsym  = mksym "symbol"
+  let mutsym  = mksym "mut"
+  let conssym = mksym "cons"
+  let nilsym  = mksym "nil"
+
+  let vtype = vfn1 (fun x d -> C((match x with
+    | I _ -> intsym
+    | S _ -> strsym
+    | Y _ -> symsym
+    | M _ -> mutsym
+    | C _ -> conssym
+    | N   -> nilsym), d))
+
+  let veq = vfn2 (fun x y d -> C(if x == y then 1 else 0, d))
+  let vcons = vfn2 (fun x y d -> C(C(x, y), d))
+  let vuncons = vfn1 (function
+    | C(C(x, y), d) -> C(x, C(y, d))
+    | x             -> raise (OhComeOn x))
+
+  let rec nth x = function
+    | 0 -> x
+    | n -> match x with
+      | C(x, xs) -> nth xs (n-1)
+      | x        -> raise (OhComeOn x)
 end
