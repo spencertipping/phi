be4f043661bd87b531c4bec136d9508fd29ceb27
Spencer Tipping <spencer.tipping@gmail.com>
2018-01-06 05:18:01 -0700

    This is just painful

--- interpreter.ml
+++ interpreter.ml
@@ -17,5 +17,26 @@ end = struct
 end
 
 module PhiInterpreter = struct
-  
+  exception OhComeOn of PhiVal.t
+
+  let mksym s  = Symbol (Hashtbl.hash s, s)
+  let mkmut () = Mutable (ref None)
+  let mkstr s  = String (Bytes.of_string s)
+
+  let interp d c r = Cons (d, Cons (c, Cons (r, Nil)))
+
+  let rewrite_fn f = function
+    | Cons (d, Cons (c, Cons (r, Nil))) -> f c d r
+    | x                                 -> raise (OhComeOn x)
+
+  let iquote   d c r = interp (Cons (interp d c r), d) c r
+  let iunquote d c r = interp d c r
+  let ieval    d c r = match d with
+    | Cons (x, d') -> interp d' (Cons (x, c)) r
+    | x            -> raise (OhComeOn x)
+
+  let vfn f d c r = interp (f d) c r
+  let vfn1 f d c r = match d with
+    | Cons (x, d') -> f x d'
+    | x            -> raise (OhComeOn x)
 end
