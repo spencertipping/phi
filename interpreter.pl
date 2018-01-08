f50ee0b745f35bbb5ec7382d55be0a49f1e6df2d
Spencer Tipping <spencer.tipping@gmail.com>
2018-01-07 17:22:37 -0700

    Better cons explain

--- interpreter.pl
+++ interpreter.pl
@@ -9,11 +9,23 @@ sub psym  { bless \my $x = $_[0], 'phi::sym' }
 sub pmut  { bless \my $x,         'phi::mut' }
 
 sub phi::nil::explain  { '[]' }
-sub phi::cons::explain { $_[0]->head->explain . " :: " . $_[0]->tail->explain }
 sub phi::int::explain  { ${+shift} }
 sub phi::str::explain  { "\"${+shift}\"" }
 sub phi::sym::explain  { ${+shift} }
 sub phi::mut::explain  { defined ${$_[0]} ? 'M[...]' : 'M[]' }
+
+sub phi::cons::explain
+{
+  my $cell = my $self = shift;
+  my @elements;
+  for (; ref $cell eq 'phi::cons'; $cell = $cell->tail)
+  {
+    push @elements, $cell->head->explain;
+  }
+  ref $cell eq 'phi::nil'
+    ? "[" . join(" ", @elements) . "]"
+    : join(" :: ", @elements, $cell->explain);
+}
 
 BEGIN
 {
