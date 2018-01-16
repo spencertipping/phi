bb49fcc03279a8cefe794b73eef490734d5f6e34
Spencer Tipping <spencer.tipping@gmail.com>
2018-01-16 09:34:10 -0700

    Experimenting with compilation strategies

--- phiapplicative.pm
+++ phiapplicative.pm
@@ -14,12 +14,42 @@ The stack layout looks like this:
 Let's talk about this in more detail.
 
 =head2 Stack layout encoding
-Suppose we're compiling an expression like C<y = f(x, x + 1)>. This looks like
-it would be straightforward to compile: we'd end up with C<x x 1 + f> or
-similar. But C<x> is already on the stack somewhere, so really we'd write out a
-sequence of constant-pushes, C<restack> instructions, and operations to arrange
-the stack correctly.
+Suppose we're compiling an expression like C<f(x, x + 1)>. This looks like it
+would be straightforward to compile: we'd end up with C<x x 1 + f> or similar.
+But C<x> is already on the stack somewhere, so really we'd write out a sequence
+of constant-pushes, C<restack> instructions, and operations to arrange the stack
+correctly.
 
-B<TODO>: is it correct for the parsers to track this? Maybe it's more sensible
-to cons up a graph first, then have that graph convert itself to concatenative.
+So let's walk through the compilation of a function in terms of the stack layout
+list:
+
+  f(x) = g(x, x + 1)
+
+  |g(x, x + 1)        [0 x]               # initially just x on the stack
+  g|(x, x + 1)        [1 0 x]             # "g" is resolved and bound to expr 0
+  g(x|, x + 1)        [2 1 0 x]           # "x" is cloned and pushed
+  g(x, x| + 1)        [3 2 1 0 x]         # ditto
+  g(x, x + 1|)        [4 3 2 1 0 x]       # "1" is pushed as expr 3...
+                      [5 4 3 2 1 0 x]     # ...and x+1 is pushed as expr 4
+  g(x, x + 1)|        [6 5 4 3 2 1 0 x]   # ...and we make the tuple (expr 5)
+                      [7 6 5 4 3 2 1 0 x] # ...and call g (expr 6)
+
+Then the function context cleans up locals by just returning the stack top; this
+is a single C<restack> operation.
+
+The stack layout gets more interesting when we have local variable bindings.
+Then we have something like this:
+
+  f(x) = let y = x + 1 in g(x, y)
+
+  |let y = x + 1 in g(x, y)     [0 x]
+  let y = x| + 1 in g(x, y)     [1 0 x]
+  let y = x + 1| in g(x, y)     [2 1 0 x] -> [3 2 1 0 x]
+  let y = x + 1 in| g(x, y)     [3 y 2 1 0 x]
+
+TODO: I'm not sure I like this strategy. We end up with a lot of leftover stack
+stuff that doesn't seem particularly necessary or productive; do we want
+parse-local concatenative management and then to store a mapping from
+named bindings to stack positions? (Ah, but what if we need to look up a
+variable I<while> we're building up the stack-local stuff? Add a depth offset?)
 =cut
