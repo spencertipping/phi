c690f3cdd18e0dae434b2cedd8a5a61d7c997954
Spencer Tipping <spencer.tipping@gmail.com>
2018-01-17 18:06:46 -0700

    More random stuff until I accumulate enough resolve to write the infix parsers properly (might as well just go for it)

--- philocal.pm
+++ philocal.pm
@@ -3,5 +3,7 @@ We want to bind stack entries to names so we can easily refer to them and place
 them without using C<restack>. To do that, we need a C<let> construct that
 stacks a custom resolver that contains local variables.
 
-
+This is the first example of a concatenative macro: we transform the
+continuation stack to restore the original resolver after the local scope ends.
+In this case that happens after the current list.
 =cut
