d78e2322e8f978b8af6f657d86fe7f393161e96d
Spencer Tipping <spencer.tipping@gmail.com>
2018-02-28 18:07:53 -0700

    Implementation strategy

--- philang.pm
+++ philang.pm
@@ -8,13 +8,6 @@ an AST node but is more aware of its runtime role; in particular, abstract
 values constant-fold at parse time and then locally alter the grammar of the
 language by specifying their own suffix parsers. Local variables are
 dereferenced at parse time.
-
-=head2 Abstracts, the interpreter, and parse states
-The compilation cycle involves a few steps:
-
-  text -> parsed <-> abstracts -> concatenative     # parsing
-                  -> abstracts -> concatenative     # optimization
-                  -> parsers -> code                # compiler backends
 =cut
 
 package philang;
@@ -29,20 +22,20 @@ use phiparse;
 use phiobj;
 
 our @EXPORT =
-our @EXPORT_OK = qw/ /;
+our @EXPORT_OK = qw//;
 
 
-=head2 Primitive abstracts
-We need abstract containers for all primitive types.
+=head2 Abstracts, the interpreter, and parse states
+The compilation cycle involves a few steps:
+
+  text -> parsed <-> abstracts -> concatenative     # parsing
+                  -> abstracts -> concatenative     # optimization
+                  -> parsers -> code                # compiler backends
+
+Implementation:
+
+1. Atom parser (derived from the parse state)
+2. Expr parser (atom >>= op nil)
+3. ???
+4. PROFIT
 =cut
-
-use constant cons_type => pmut;
-use constant int_type  => pmut;
-use constant nil_type  => pmut;
-use constant str_type  => pmut;
-use constant sym_type  => pmut;
-use constant mut_type  => pmut;
-
-use constant abstract_int => mktype
-  bind('is-const' => drop, lit 1),
-  bind('is-crash' => drop, lit 0);
