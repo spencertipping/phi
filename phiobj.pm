510f8c5659bf786398d314aed3afae459b890836
Spencer Tipping <spencer.tipping@gmail.com>
2018-03-26 05:57:12 -0600

    Aha, safe method calls fail in C

--- phiobj.pm
+++ phiobj.pm
@@ -75,7 +75,7 @@ our @EXPORT_OK =
 
 # Great for debugging
 use constant TRACE_METHOD_CALLS => 0;
-use constant SAFE_METHOD_CALLS  => 1;
+use constant SAFE_METHOD_CALLS  => 0;
 
 
 =head2 Type constructor
