914919cbfbd43ea998dd33282d78f8c4a8f161f4
Spencer Tipping <spencer.tipping@gmail.com>
2018-03-27 08:15:30 -0600

    Doing licenses the correct way I guess? http://www.gnu.org/licenses/gpl-howto.html

--- delta.md
+++
@@ -1,4 +0,0 @@
-# Next steps
-1. Convert `philang` and `phifront` to dialects
-2. Get REPL unit tests to pass
-3. Party like it's 1999
--- phiops.pm
+++ phiops.pm
@@ -1,3 +1,20 @@
+=head1 License
+    phi programming language
+    Copyright (C) 2018  Spencer Tipping
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 =head1 phi prefix/infix/postfix operator support
 Alright, let's define some actual syntax now that C<philang> works. Parse
 continuations are technically managed by values rather than types, but in
