59bdec4a6d5aae816d401bd544a87833d73a83a8
Spencer Tipping <spencer.tipping@gmail.com>
2018-04-24 10:32:34 -0600

    WIP phidata

--- phidata.pm
+++ phidata.pm
@@ -17,4 +17,49 @@
 
 
 =head1 phi data structures
+We could use lists for everything, but it would be horrifically slow. So let's
+define some data structures to fix this. The most important of these is
+C<array>, which uses bisection lists to get C<O(log n)> access/update time per
+element.
 =cut
+
+
+package phidata;
+use strict;
+use warnings;
+
+use Exporter qw/import/;
+use phiboot;
+use phibootmacros;
+use phiobj;
+use philist;
+
+our @EXPORT =
+our @EXPORT_OK = qw/ array_type array_from_list /;
+
+
+=head2 Array data structure
+The idea here is pretty simple: each bit of the index specifies head/tail. We
+store the tree depth in the root object and pass that into the bisection
+function.
+=cut
+
+use phi array_get_mut => pmut;
+use phi array_get => l                  # c n levels
+  dup,                                  # c n levels more?
+  l(                                    # c n levels
+    lit 1, i_neg, i_plus,               # c n levels-1
+    stack(0, 1, 0),                     # c n levels-1 levels-1 n
+    i_rsh, lit 1, i_and,                # c n levels-1 t|h
+    l(rot3l, tail),
+    l(rot3l, head),
+    if_,                                # n levels-1 c'
+    rot3r, array_get_mut, i_eval),      # array_get(c', n, levels-1)
+  l(                                    # c n 0
+    stack(2)),                          # c
+  if_;
+
+array_get_mut->set(array_get);
+
+
+1;
