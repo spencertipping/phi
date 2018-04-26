a071b8c22622b17124aabb228f4750273bff6b87
Spencer Tipping <spencer.tipping@gmail.com>
2018-04-26 02:39:50 -0600

    WIP bisection tree + array data type

--- phidata.pm
+++ phidata.pm
@@ -47,7 +47,7 @@ C<bisection_get> and C<bisection_update>.
 A bisection tree looks like this in practice:
 
   # make a bisection tree of length 5 for the list [1, 2, 3, 4, 5]
-  tree = ((1 :: 2) :: (3 :: 4)) :: ((5 :: nil) :: (nil :: nil))
+  tree = ((1 :: 2) :: (3 :: 4)) :: ((5 :: nil) :: nil)
 
 Then each access index is decomposed into bits to bisect the tree in C<log(n)>
 time:
@@ -76,7 +76,7 @@ use phi bisection_get => l              # c n levels -> x
     stack(2)),                          # c
   if_;
 
-bisection_get_mut->set(bisection_get);
+BEGIN { bisection_get_mut->set(bisection_get) }
 
 
 use phi bisection_update_mut => pmut;
@@ -86,12 +86,105 @@ use phi bisection_update => l           # x c n levels -> c'
     lit 1, i_neg, i_plus,               # x c n levels-1
     stack(0, 1, 0),                     # x c n levels-1 levels-1 n
     i_rsh, lit 1, i_and,                # x c n levels-1 bit
-    # TODO
-    ),
+    l(                                  # x c n levels-1
+      rot3l, i_uncons,                  # x n levels-1 ct ch
+      stack(5, 2, 3, 1, 4, 0),          # ch x ct n levels-1
+      bisection_update_mut, i_eval,     # ch ct'
+      swons),                           # c'
+    l(                                  # x c n levels-1
+      rot3l, i_uncons,                  # x n levels-1 ct ch
+      stack(5, 2, 3, 0, 4, 1),          # ct x ch n levels-1
+      bisection_update_mut, i_eval,     # ct ch'
+      i_cons),                          # c'
+    if_),                               # c'
   l(stack(3)),                          # x
   if_;
 
-bisection_update_mut->set(bisection_update);
+BEGIN { bisection_update_mut->set(bisection_update) }
+
+
+=head3 Bisection tree allocation
+I'm afraid this is cheating. Since this is an immutable data structure, we can
+preallocate everything using aliased children and then use a bisection list to
+store the preallocated things. Then "allocation" is just C<lists[n]>, where C<n>
+is the required bit depth. This is C<O(log n)> in the bit depth itself, which is
+as fast as we can possibly go.
+
+Now there's one complication here, and it's a dumb one: how do we know how many
+integer bits we have for this runtime? I'm end-running this for now by assuming
+that we always have 64 bits max, but this isn't a very elegant assumption and at
+some point in the future it's likely to be false.
+=cut
+
+use phi bisection_level_mut => pmut;
+use phi bisection_level => l            # level -> c
+  dup,                                  # level bisect?
+  l(                                    # level
+    lit 1, i_neg, i_plus,               # level-1
+    bisection_level_mut, i_eval,        # cnext
+    dup, i_cons),                       # cnext::cnext
+  l(drop, pnil),                        # nil
+  if_;                                  # c
+
+BEGIN { bisection_level_mut->set(bisection_level) }
+
+
+use phi bisections_preallocated => le   #
+  lit 64, bisection_level, i_eval,      # c(64)
+  lit 6,  bisection_level, i_eval,      # c(64) r
+  l(                                    # recur x r i
+    dup,                                # recur x r i nonzero?
+    l(                                  # recur x r i
+      # Store r[i] = x, then recur with x' = x.head, i' = i-1
+      stack(3, 0, 1, 2, 0, 2),          # recur x i x r i
+      lit 6,                            # recur x i x r i 6
+      bisection_update, i_eval,         # recur x i r'
+      swap, lit 1, i_neg, i_plus,       # recur x r' i'
+      rot3l, head, rot3r,               # recur x' r' i'
+      stack(0, 3), i_eval),             # recur(recur, x', r', i')
+    l(                                  # recur x r i
+      # There's no such thing as a bisection tree of level 0, so we don't have
+      # to do anything here; we can just return r.
+      stack(4, 1)),                     # r
+    if_),                               # x r f
+  rot3r, lit 63,                        # f x r i
+  stack(0, 3), i_eval;                  # r'
+
+
+use phi bisection_new => l              # levels -> c
+  bisections_preallocated, swap,        # xs levels
+  lit 6, bisection_get, i_eval;         # xs[levels]
+
+
+=head3 Fast integer log
+TODO
+=cut
+
+use phi integer_log => l                # n
+  ;
+
+
+=head2 Array data structure
+An OOP wrapper around bisection trees. We store the depth on the object to
+reduce the amount of overhead involved.
+=cut
+
+use phitype array_type =>
+  bind(bisection_tree      => isget 0),
+  bind(bisection_levels    => isget 1),
+  bind(with_bisection_tree => isset 0),
+
+  bind(get =>                           # i self -> self[i]
+    dup, mcall"bisection_tree",         # i self c
+    rot3r, mcall"bisection_levels",     # c i levels
+    bisection_get, i_eval),             # x
+
+  bind(update =>                        # x i self -> self'
+    dup, mcall"bisection_tree",         # x i self c
+    nip, mcall"bisection_levels",       # x i self c levels
+    stack(5, 0, 3, 1, 4, 2),            # self x c i levels
+    bisection_update, i_eval,           # self c'
+    swap, mcall"with_bisection_tree");  # self'
 
 
 1;
