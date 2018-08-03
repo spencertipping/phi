54453b8cc7b431da0d511baa578a28d7a39031cf
Spencer Tipping <spencer.tipping@gmail.com>
2018-08-03 08:00:03 -0600

    Nuking more stuff. It isn't yet clear how the assembler should be structured, nor is it clear whether we'll even end up with an assembler per se.

--- phi1back/flow.pm
+++ phi1back/flow.pm
@@ -80,34 +80,6 @@ phi's CTTI is more involved than CTTI in most languages for two reasons:
 
 1. CTTI bends the grammar at parse time
 2. As a result, we can't trivially inspect expressions in a normal form
-
-(2) matters because correct GC atomicity demands that we not only store any
-subexpressions used by the continuation, but also, ideally, that we clear them
-as soon as they _aren't_ used. (It also throws a number of monkey wrenches into
-the picture for traditional compiler stuff like lambda closure conversion. I'll
-get into how we deal with these problems a bit below.)
-
-
-=head3 Assembler structure
-Assemblers are linked lists, basically. You start with a nil base link and cons
-new stuff onto it, holding onto those new links. These conses proceed
-rightwards, so the interpretation is like this:
-
-  (((nil swap) dup) drop)       -> [swap dup drop]
-
-There are several different kinds of assembler links:
-
-1. Low-level stack instruction (like the above)
-2. Insert a constant
-3. Set CTTI of stack
-4. Set CTTI of frame
-5. Set CTTI of interpreter
-
-These link types are enough to fully represent the low-level operational
-semantics of phi, but they're lossy: just as C<bin> lossily compiles method
-calls down to their constituent C<dup m64get ...> operations, the links encoding
-a method call don't contain information about which method call was made, or
-even that one was made at all. Nor should they.
 =cut
 
 
