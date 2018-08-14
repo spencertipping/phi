a59bf7d2c6fbd7d5a1113dc4242645202b3f58c3
Spencer Tipping <spencer.tipping@gmail.com>
2018-08-14 06:08:50 -0600

    Figuring out how the frontend parse protocol and negotiation work. Link design seems right, but the parse state stuff could be improved I think.

--- phi1front/phi2.pm
+++ phi1front/phi2.pm
@@ -42,10 +42,60 @@ those methods look like operators, then those can be integrated into the syntax
 as such (and at the appropriate precedence for the dialect in question).
 
 Put differently, CTTIs are at liberty to rely solely on the method/virtual
-tables to convey functionality, and be confident that any frontend will adapt
-those in a sane way.
+tables to convey functionality and they can be confident that any frontend will
+present those in a sane way.
 
 
+=head3 Assembling expressions
+Most frontends (including phi2) convert expressions to something close to ANF to
+flow-assemble them. So we'd have something like this:
+
+  # if we compile this:
+  x + y.bar(bif + baz, bok.bork())
+
+  # ...we'd compile it like this:
+  let tmp1 = bif + baz in
+  let tmp2 = bok.bork() in
+  let tmp3 = y.bar(tmp1, tmp2) in
+  let tmp4 = x + tmp3 in
+  tmp4
+
+C<let>-bindings don't dynamically extend the scope; rather, phi2 finds them up
+front and builds a frame class for the full set of referenced variables. That
+makes it important for C<tmpN> variables to be unique gensyms in their own
+namespace.
+
+Because scopes aren't always defined in parse order, we use parsers to stage
+flow assembler links. That is, parsers return objects that do two things:
+
+1. Return a map of C<< name -> CTTI >> entries to describe their definition set
+2. Return a set of C<name> entries to describe their reference set
+3. Add new links to an existing flow assembler
+
+NB: these links are right-inclusive, just like the tail of a cons cell. There is
+no tail-tail or "rest of the list" otherwise.
+=cut
+
+use constant phi2_link_protocol => phi::protocol->new('phi2_link',
+  qw/ defset
+      refset
+      link_onto /);
+
+
+=head3 Parse state
+Frontend parse states are slightly nontrivial because they have a bit of
+negotiation to do. In particular, not all frontends support:
+
+1. Lexical scoping and lambda-capture
+2. Dynamic scoping
+3. Prefix-free variable addressing
+4. Variable addressing at all
+
+We care about this stuff because CTTIs can operate as parsers to provide
+computed grammars, so they need a standard way to interoperate with the
+frontend's parse state.
+
+TODO: design the negotiation protocol
 =cut
 
 
