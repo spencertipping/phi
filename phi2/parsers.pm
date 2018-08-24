48c4dbb13d70ccedc84c038747c1ccf4bc440a51
Spencer Tipping <spencer.tipping@gmail.com>
2018-08-24 07:30:36 -0600

    Oops, fix comment

--- phi2/parsers.pm
+++ phi2/parsers.pm
@@ -76,7 +76,6 @@ use phi::genconst decimal_integer => bin q{
   [ _ =10 itimes                        # x cc x0*10
     sget02 =48 ineg iplus iplus sset01  # x0*10+x cc
     goto ]                              # digit init next
-
   [ goto ]                              # digit init next last
   prep                                  # p };
 
@@ -84,7 +83,7 @@ use phi::testfn decimal_integer => bin q{
   "0" =0 strpos decimal_integer .parse
   .value =0 ieq "int0" i.assert
 
-  "65536" =0 strpos decimal_integer .parse # {v=12345}
+  "65536" =0 strpos decimal_integer .parse # {v=65536}
   dup .fail? inot "int_nofail" i.assert
   dup .value lit32 00010000 ieq "int65536" i.assert
   drop                                  # };
