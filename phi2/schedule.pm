15834beb652fbb777db0bcc67fd8beb82b5d1698
Spencer Tipping <spencer.tipping@gmail.com>
2018-09-16 08:50:00 -0600

    Initial schedule implementation (untested). I think I want some constructor methods too, but I'll need to know more about the CTTI side before I write it.

--- phi2/schedule.pm
+++ phi2/schedule.pm
@@ -29,8 +29,60 @@ Put differently, not all expressions can be represented using a single C<ir_bb>.
 We need to manage linked structures of C<ir_bb>s as a single unit so we can
 return that structure from the dialect's C<expression> parser.
 
-TODO: go through an example that involves branching
+For example, an expression like C<f(x) ? g(x) : h(x)> involves three basic
+blocks: one to calculate C<f(x)> and then branch, one to calculate C<g(x)>, and
+one to calculate C<h(x)>. The schedule for this expression allows us to then
+specify its continuation.
+
+A schedule also indicates which local slot represents its value. For instance,
+C<f(g(x))> creates two local slots but only returns the one for C<f(...)>.
+
+Here's the struct:
+
+  struct schedule
+  {
+    hereptr class;
+    ir_fn*  fn;
+    int32   entry;                      # index of initial basic block
+    int32   exit;                       # index of final basic block
+    int32   val;                        # index of local variable
+  };
+
 =cut
+
+use phi::protocol schedule =>
+  qw/ fn
+      entry
+      exit
+      val /;
+
+use phi::protocol schedule_linkage =>
+  qw/ entry_block
+      exit_block
+      +=
+      << /;
+
+use phi::class schedule =>
+  schedule_protocol,
+  schedule_linkage_protocol,
+
+  fn    => bin q{ _ =8  iplus m64get _ goto },
+  entry => bin q{ _ =16 iplus m32get _ goto },
+  exit  => bin q{ _ =20 iplus m32get _ goto },
+  val   => bin q{ _ =24 iplus m32get _ goto },
+
+  entry_block => bin q{ sget01 .entry sget02 .fn .blocks .[] sset01 goto },
+  exit_block  => bin q{ sget01 .exit  sget02 .fn .blocks .[] sset01 goto },
+
+  '<<' => bin q{ sget02 sget02 .exit_block .<< drop sset01 goto },
+  '+=' => bin q{                        # rhs self cc
+    # Link our exit block to the RHS's entry block and update this schedule to
+    # return the RHS.
+    sget02 .entry =0_ dup ir_branch     # rhs self cc if(0,bbi,bbi)
+    sget02 .exit_block .<< drop         # rhs self cc
+    sget02 .exit sget02 =20 iplus m32set  # [self.exit=]
+    sget02 .val  sget02 =24 iplus m32set  # [self.val=]
+    sset01 _ goto                       # self };
 
 
 1;
