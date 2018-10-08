3bcfa9f52eda1de4b0ec3dfc5e964c49e9d2dab2
Spencer Tipping <spencer.tipping@gmail.com>
2018-10-08 01:29:04 -0600

    WIP rewriting timeline spec to reflect new design. It seems like there's something awful lurking underneath the parameterized-timeline approach, but I don't know what it is yet.

--- phi2/timeline.pm
+++ phi2/timeline.pm
@@ -23,108 +23,37 @@ use warnings;
 
 
 =head2 Timelines
-I'm not quite sure how this works, so I guess I'll start with what's obviously
-true:
+Code doesn't directly map to an evaluation schedule; there are too many degrees
+of freedom to commit to a single ordering, and not all programs that are parsed
+are intended to be evaluated at all. Instead, the parse output is something more
+generally informative: a set of functional dependencies that describe the
+calculations required to execute the code. The degree of sequencing is encoded
+by the dialect and the abstracts, and collectively the calculation set is a
+graph called a timeline.
 
-1. Inlined functions produce timelines computed from arguments
-2. Non-inlined functions produce some sort of polymorphic call/return structure
-3. Timelines are values, often with no left-links (i.e. they're constant)
-4. Timelines can be computed at runtime, in which case we have JIT
-5. The compiler is a function from stuff to timelines
-6. C<root> can flatmap timelines
-7. Optimization is timeline algebra
+Functionally speaking, timelines are phi's intermediate representation and form
+a variation of CPS that specifies which expressions are temporally independent.
+More broadly:
 
-phi is an imperative language, which means that regardless of the paradigm we're
-presenting through any given dialect we ultimately need to commit to some side
-effect schedule that encodes the semantics we want. We do this with timelines.
+1. Timelines can be trivially converted to bytecode
+2. Bytecode produced by a timeline links back to the timeline object
+3. Backend-specific JIT compilers use timelines as input
+4. Bytecode can be trivially converted to a GC-unsafe timeline
+5. Timelines preserve/store CTTI
+6. Timelines preserve/store all method calls, including monomorphic ones
+7. Timelines can be parameterized on unspecified quantities
 
-Timelines are branching structures; if two events need to happen and we don't
-care about their relative ordering, then we have a fork and a join:
+(5) and (6) are important for algebraic analysis. Basically, the idea is that we
+want timelines to be a lossless semantic encoding of the source they came from.
+This means no type or intent erasure: if we have a situation where an abstract
+defines two methods that compile to the same underlying function, we should
+still be able to know which one was called.
 
-                event A
-               /       \
-  ...timeline .         . timeline...
-               \       /
-                event B
-
-Backend interpreters or compilers can then choose an arbitrary ordering of A and
-B, or can execute both at once (e.g. on different processors or machines).
-
-
-=head3 Speculation
-phi begins with a single timeline C<root> that represents global program actions
-that are ultimately visible to the kernel. Anything you merge into this timeline
-is guaranteed to happen, which means it's committed: you can't undo these
-merges.
-
-Not every timeline ultimately needs to be committed. If you write an expression
-that does a bunch of computation but doesn't create any side effects, that
-timeline can remain unmerged from C<root> until you entangle its result. For
-example:
-
-  int x = 5;                            # no root entanglement
-  int y = x + 1;                        # ditto
-  int z = y > 5 ? y : 9;                # ditto
-  int q = print(z);                     # z is entangled here
-
-Structurally speaking, the above looks like this (if we treat C<print> as being
-opaque):
-
-   ;     ;             ;          ;     <- sequence points
-   |     |             |          |
-  5|x-\  |          9  |          |
-      [+]|y-\      / \ |          |
-  1---/      [< if]   .|z--[print]|q--
-            /      \ /    /       \
-  5--------/        y    /         \
-                        /           \
-  root-----------------/             \----root'
-
-C<root> doesn't fork to C<z>'s timeline because C<z> doesn't depend on C<root>
-at all. This means we can schedule it anywhere before C<print(z)>, including at
-compile time.
-
-...so basically, a link leftwards from B to A should indicate that A is the
-earliest moment when B becomes computable.
+(7) is questionable...
 
 
-=head3 Specialization
-Timelines tell us when we can evaluate things. If an expression has no
-left-facing links -- i.e. no dependencies -- then its meaning is time-invariant;
-C<3 + 4> always means 7, regardless of where we are on the global timeline.
+=head3 Sequence arguments
 
-Specialization happens when we transform timelines in a way that removes
-left-facing links. There are a few ways this can happen:
-
-1. Forward propagation -- i.e. abstract interpretation
-2. Speculation, e.g. simulating both directions of an C<if>
-3. On demand, e.g. profile-guided JIT
-
-Technically (3) is always happening; left links effectively vanish as
-expressions are evaluated.
-
-
-=head3 Timelines as first-class values
-C<root> is objective only from the inside; if there are two phi runtimes, each
-will have a C<root> and synchronization/delegation happen over some sort of RPC.
-
-Another way to say it is that timelines are applied functions. Saying "hey
-remote, merge this timeline" is exactly the same as saying "hey remote, call
-this function" -- except that timelines, being first-class values, can
-interleave their execution in various ways, including within the same serial
-runtime. This is useful for things like embedded programming, where you can
-simulate multithreaded processing within a single thread by having timelines
-that interleave instructions and track the cycle counts to guarantee latencies
-(synthetic interrupts).
-
-Functionally speaking, timelines do two things:
-
-1. Provide code to execute them
-2. Describe their structure for algebraic reduction
-
-
-=head3 Timeline structure
-Q: are we thinking about _events_ or _timelines_?
 
 =cut
 
