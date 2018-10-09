5209fccdef05c58f09630d9206d1dd24caa3117d
Spencer Tipping <spencer.tipping@gmail.com>
2018-10-09 05:48:35 -0600

    More timeline design; this is starting to take shape. I think the next step is to figure out how to do structural rewriting.

--- phi2/timeline.pm
+++ phi2/timeline.pm
@@ -53,6 +53,51 @@ still be able to know which one was called.
 confined to a static compilation model.
 
 
+=head3 Timeline nodes
+Timelines are made of these types of nodes:
+
+1. C<< const(X, ctti)               -> value, ctti >>
+2. C<< arg(name, ctti)              -> value, ctti >>
+3. C<< return(name, value, ctti) >>
+4. C<< method(ctti, m)              -> timeline(...) >>
+5. C<< code(X, [inputs], [outputs]) -> timeline([inputs], [outputs]) >>
+6. C<< call(timeline, args...)      -> v1, v2, ..., vN >>
+7. C<< goto(timeline, args...) >>
+
+NB: C<arg> and C<return> seem like hacks, but they aren't. C<call> and C<goto>
+relink those nodes when we want to make an inline function call.
+
+Nodes are multiway, both for input and output. That is, any given node has
+multiple input and output linkage points, and canonical linkages always point
+towards dependencies:
+
+  method-----------+       call-------------+
+  |                |       |                |
+  |ctti            |       |            ret1|<--- ...
+  |        timeline|<------|timeline    ret2|<--- ...
+  |method          |       |             ...|<--- ...
+  |                | +-----|arg1            |
+  +----------------+ | +---|arg2            |
+                     | |   +----------------+
+     const(5)-+      | |
+     |   value|<-----+ |
+     +--------+        |
+                       |
+     const(0)-+        |
+     |   value|<-------+
+     +--------+
+
+Then we run a pass to calculate forward links. These are always derived from
+backlinks as a form of garbage collection; this simplifies dead-code
+elimination and makes the API less redundant overall.
+
+Optimization parsers also run in reverse. These parsers need to know when a node
+has multiple references against one of its outputs: if so, then a multi-node
+optimization needs to either duplicate the node in question, or fail to optimize
+it. (Multiple references mean that an intermediate result is used for two
+different purposes, so we can't do anything that eliminates it.)
+
+
 =head3 Sequence arguments and side-effect domains
 Like Haskell, phi relies on functional dependencies for scheduling and sequence
 points. Every side-effecting function takes a "sequence argument" and returns
@@ -112,53 +157,82 @@ timeline. Then all side effects against it are serialized.
 Back to array modification though: C<array.set(i, x)> involves some timeline
 owned by the array. How is this encoded? We have no functional dependency on
 C<root>, but we _do_ have a functional dependency on the original array
-timeline. Any dialect generating a timeline for an array needs to know about
-this.
+timeline. Any dialect managing an array's timeline needs to know about this.
+
+I think we can get away with a lot:
+
+  int xs[3];
+  xs[0] = 10;                           # xs.t(0)
+  xs[1] = 20;                           # xs.t(1)
+  xs[2] = 30;                           # xs.t(2)
+  int total = 0;
+  for i : xs.indexes
+  {
+    total += xs[i];                     # total ^ xs.t(i)
+  }
+
+Here's the timeline we want, topsorted by dependencies. I'm drawing the arrows
+pointing towards outputs just because it's more intuitive. Note that I'm
+assuming C<i> is a fictitious variable, C<xs.t(i)> is 1:1 with C<i>, and the
+loop is fully unrolled.
+
+  [heap] { heap, xs <- array.new(heap, 3, int),
+           total0   <- 0 }
+  [xs] { xst0 <- xs.t(0),
+         xst1 <- xs.t(1),
+         xst2 <- xs.t(2) }
+  [xs, xst0, xst1, xst2] { xst0 = xs.set(xst0, 0, 10),
+                           xst1 = xs.set(xst1, 1, 20),
+                           xst2 = xs.set(xst2, 2, 30) }
+  [xs, xst0, total0] { total1 <- total0.+(xs.get(xst0, 0)) }
+  [xs, xst1, total1] { total2 <- total1.+(xs.get(xst1, 1)) }
+  [xs, xst2, total2] { total  <- total2.+(xs.get(xst2, 2)) }
+
+The only initial dependency is on some heap, but we don't care whether it's
+compile-time or runtime. C<xst0> etc are sequence arguments to track
+modifications to individual elements of C<xs>.
+
+We do the normal SSA/CPS thing with numbered revisions of C<total>. In practice
+these wouldn't have names, but C<int> is an immutable type so we don't need to
+track modifications; we can just refer to functional transformations, in this
+case driven by C<int::+>.
 
 
-=head3 Timeline nodes
-Timelines are made of these types of nodes:
+=head3 Sequence argument constructors
+In the example above, C<xs.t(i)> returns a sequence argument to track
+modifications of C<xs[i]>. In practice this is a non-value; it just needs to be
+present for sequencing. Let's get into this a bit.
 
-1. C<< const(X)                     -> value, ctti >>
-2. C<< arg(i)                       -> value, ctti >>
-3. C<< method(ctti, m)              -> timeline(...) >>
-4. C<< code(X, [inputs], [outputs]) -> timeline([inputs], [outputs]) >>
-5. C<< call(timeline, [args])       -> v1, v2, ..., vN >>
-6. C<< goto(timeline, [args]) >>
+First, C<xs.t(i)> is a self-contained domain: it's unrelated to C<root>,
+C<heap>, and everything else except for C<xs>. This means we can sequence it
+anywhere we want to. The fact that C<xs.t(i)> takes no external arguments means
+that arrays have a confined algebra of some kind. More specifically, it means we
+can simulate their behavior without committing to things.
 
-Nodes are multiway, both for input and output. That is, any given node has
-multiple input and output linkage points, and canonical linkages always point
-towards dependencies:
+Second, C<xs.set(t, i, x)> never interacts with C<root> because arrays are
+process-subjective; they aren't real to anyone outside the current process. This
+means we can eliminate array assignments to elements we don't subsequently
+access. Proving this may be difficult, but it's an option.
 
-  method-----------+       call-------------+
-  |                |       |                |
-  |ctti            |       |            ret1|<--- ...
-  |        timeline|<------|timeline    ret2|<--- ...
-  |method          |       |             ...|<--- ...
-  |                | +-----|arg1            |
-  +----------------+ | +---|arg2            |
-                     | |   +----------------+
-     const(5)-+      | |
-     |   value|<-----+ |
-     +--------+        |
-                       |
-     const(0)-+        |
-     |   value|<-------+
-     +--------+
+At this point it's natural to ask how C<xst0> is any different from a local
+variable that's acting as a less-C<volatile> cache of C<xs[0]>. Practically
+speaking, that's exactly what it is; but we get to use memory that's managed by
+a data structure. The real difference is that C<xs.t> can either use or ignore
+its index argument -- and this happens monomorphically, which in turn influences
+the way the dialect will create timelines.
 
-Then we run a pass to calculate forward links. These are always derived from
-backlinks as a form of garbage collection; this simplifies dead-code
-elimination and makes the API less redundant overall.
+C<xs> has a timeline of its own, and we can synchronize that to individual
+elements (commit the elements) by merging it:
 
-Optimization parsers also run in reverse. These parsers need to know when a node
-has multiple references against one of its outputs: if so, then a multi-node
-optimization needs to either duplicate the node in question, or fail to optimize
-it. (Multiple references mean that an intermediate result is used for two
-different purposes, so we can't do anything that eliminates it.)
+  xst = xs.merge(xst, xst0);            # xs[0] is now up to date
+  xst = xs.merge(xst, xst1);            # xs[1] is now up to date
+  xst = xs.merge(xst, xst2);            # xs[2] is now up to date
 
+Note that like C<xs.t()>, this operation is completely fictitious: C<xs.merge>
+generates no code. Moreover, C<xst0> and other sequence arguments also don't
+exist; their CTTI is specified as occupying no space. All of this machinery is a
+purely compile-time abstraction we use to order side effects correctly.
 =cut
-
-
 
 
 1;
