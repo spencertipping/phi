d18f5965f06f7d8f1a28ef42c64f9f777db05223
Spencer Tipping <spencer.tipping@gmail.com>
2018-10-14 04:45:10 -0600

    Hmm, having some mixed feelings about the GC implications of dialect/semantic coupling

--- dev/history/8a14-simplify.pm
+++ dev/history/8a14-simplify.pm
@@ -31,6 +31,8 @@ This carries some benefits:
 3. Dialects can fully dictate control flow and laziness/strictness
 4. Dialects can opt into disclosure about locals/globals/etc
 5. Dialects can use nonstandard calling conventions
+6. Parse states can manage their own IR tracking
+7. Dialects can dictate memory management
 
 (4) is a mixed blessing, but I think it gives us better fidelity.
 
@@ -39,5 +41,24 @@ it's a dealbreaker. I think we can expect to ask a dialect how we should call
 its functions, or have it publish its functions as objects that manage any
 calling-convention differences.
 
+(7) solves a long-running problem I've had with dialects like C, which need to
+provide pointers to things. The problem with pointers is that they break GC: if
+you have an C<int*> that refers to the field of some struct, how do we know
+which struct it belongs to and how to relocate it if a GC happens? We can't just
+add GC to any language arbitrarily.
+
 ...basically, dialects own most semantics and unify on the surface rather than
 expecting to conform at a structural level.
+
+
+=head2 Bridging semantic differences between dialects
+Obviously we should have a standard set of protocols dialects can opt into, but
+beyond that, how do we deal with more fundamental differences like memory
+management? Can we meaningfully have an unmanaged dialect that interoperates
+with a managed one? (Sure, but we need to be careful about GC pinning.)
+
+This seems to imply that GC is a thing data structures opt into in order to
+comply with managed dialects. That seems reasonable enough. We'll get a
+compile-time error if we try to use an unmanaged data type from a managed
+language (we hope). Managed datatypes from unmanaged languages might also throw
+an error depending on expectations around destructors.
