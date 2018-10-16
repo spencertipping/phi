0e89839d31d1b4bc0e3dedb6ecc7a9ff20bd0c32
Spencer Tipping <spencer.tipping@gmail.com>
2018-10-16 02:13:57 -0600

    Figuring out how this all works; this is beautiful

---
+++ dev/history/8a16-frontiers.pm
@@ -0,0 +1,27 @@
+=head1 Frontiers (a stream of consciousness firehose)
+Let's abandon all pretense of normal language function for a second.
+
+What I care about is precisely defining the division between dialects and
+abstracts. Dialects are more significant than just flavors-of-language; they're
+also flavors-of-interconnections -- really, they pick up the residual semantics
+from abstracts. Abstracts are the values being driven by dialects.
+
+We can look at dialects in a few ways:
+
+1. Dialects are abstract fabric
+2. Dialects are search space fabric
+3. Dialects are a network for abstracts
+
+I mean, these are all basically the same thing -- but none imply the usual
+linkages that frame-oriented languages tend to have. And that's what interests
+me: we have stuff like search spaces. If a dialect drives a solution search,
+then the active frame is the frontier.
+
+
+=head2 Parse time vs runtime
+This is a metaclass-vs-class distinction. Metaclasses share an inheritance
+relationship with classes.
+
+Frontier metaclasses are a dialect's interface with the rest of the world and
+with each other. I think it's that simple. I think we can also use these
+metaclasses as possibly-mutable parse states.
