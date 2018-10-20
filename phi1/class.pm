d02db69de922bb8bf881307981e4d95733dfd0bc
Spencer Tipping <spencer.tipping@gmail.com>
2018-10-20 06:10:27 -0600

    WIP definitions for classes and functions-as-objects

--- phi1/class.pm
+++ phi1/class.pm
@@ -36,7 +36,45 @@ used by classes. This is an important point because all objects in phi support a
 handful of methods like C<gc_mark> and C<class>, and these methods are managed
 by the C<phi_gc_class> metaclass rather than inherited from a base as they would
 be in most languages.
+
+
+=head2 Class description
+For phi1's purposes, a class is simply an associative list of method hashes and
+functions. Class objects are mutable, so they hold a reference to the
+associative list and reallocate it when it runs out of capacity. More
+specifically:
+
+  struct class
+  {
+    hereptr<fn>      class;
+    int              isize;
+    ptr<string>      name;
+    ptr<class_array> methods;
+  };
+
+  struct class_array
+  {
+    hereptr<fn>                      class;
+    int                              capacity;
+    int                              size;
+    pair<int, hereptr<fn>>[capacity] xs;
+  };
+
 =cut
+
+our %class_methods = (
+  class => phi::fn->sset->C(1)->swap->go->endfn,
+);
+
+our $class_class       = phi::asm->new('class_class');
+our $class_class_array = phi::asm->new('class_class_array');
+
+$class_class->patch(class_class_fn_hereptr => 8)
+            ->Ql(32)
+            ->Ql(0)
+            ->Ql($class_class_array);
+
+
 
 
 1;
--- phi1/fn.pm
+++ phi1/fn.pm
@@ -1,5 +1,3 @@
-#!/usr/bin/env perl
-
 =head1 License
     phi programming language
     Copyright (C) 2018  Spencer Tipping
@@ -20,34 +18,35 @@
 
 package phi;
 
-use v5.14;
 use strict;
 use warnings;
 use bytes;
 
-BEGIN { push @INC, $0 =~ s/\/[^\/]+$//r }
 
-use phi0;
+=head1 Function objects
+Fairly straightforward; we just need to write a header that accurately stores
+the code length. Here's the struct:
 
-use phi1::asm;
-use phi1::fn;
-use phi1::oop;
-use phi1::class;
-use phi1::frame;
-use phi1::sexp;
+  struct fn
+  {
+    hereptr    class;
+    int        size;
+    here_marker;
+    byte[size] code;
+  };
 
-# TEST CODE
-our $ihereptr     = phi::asm->new->str($phi::bytecode_table);
-our $syscall_code = phi::asm->new->str($phi::syscall_native);
-our $ok_string    = "phi1 is a thing\n";
-our $ok_addr      = phi::asm->new->str($ok_string);
-our $ok_len       = length $ok_string;
+Because C<phi::asm> instances build code incrementally, we won't know the size
+until the end. We rely on users to call C<endfn> to patch the size in.
+=cut
 
-our $code = phi::asm->new
-  ->l(0)->l(0)->l(0)->l($ok_len)->l($ok_addr)->l(1)
-  ->l(1)->l($syscall_code)->back
-  ->drop
-  ->l(0)->l(0)->l(0)->l(0)->l(0)->l(0)
-  ->l(60)->l($syscall_code)->back;
+sub fn { phi::asm->new(@_)->patch(fn_class_fn_hereptr => 8)->Ql(0)->here }
 
-print heap_image $ihereptr->addr, $code->addr;
+sub phi::asm::endfn
+{
+  my $self = shift;
+  substr($$self{data}, 8, 8) = pack Q => length $$self{data};
+  $self;
+}
+
+
+1;
