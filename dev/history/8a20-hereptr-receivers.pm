38ae7ab0b739327f3bb4c8e0ef14a6d58cfe7fdd
Spencer Tipping <spencer.tipping@gmail.com>
2018-10-20 01:42:52 -0600

    Changed the default method calling protocol to convey information about which receiver is being addressed. Do we want to standardize endianness of here markers?

--- dev/history/8a20-hereptr-receivers.pm
+++ dev/history/8a20-hereptr-receivers.pm
@@ -22,3 +22,35 @@ example. GC transparency means that if I call C<.gc_mark()> on a hereptr, I want
 another equivalent hereptr to the new object. There are other use cases too; if
 I address an element of a packed array, the packed array needs to know which
 item I'm referring to. C<unhere> erasure makes this impossible.
+
+
+=head2 Solutions
+1. Extend default method calls to include both base and offset
+2. Type all pointers as hereptrs, prepend a zero marker to object bases
+
+(2) requires more runtime memory, so let's go with (1). Now every method call
+will see this in stack terms:
+
+  obj.method(args...)                   # args... offset self cc
+
+C<offset == 0> for baseptrs and C<offset == here_marker_value> for hereptrs.
+It's the caller's job to push this quantity.
+
+
+=head2 Tagged here-markers
+Objects are unlikely to care much about the literal offset-from-base of the
+hereptr being addressed; it doesn't carry much semantic information by itself.
+But we can adopt idioms like pre-heremarker tagging to encode information about
+each receiver in the preceding bytes. That is:
+
+  struct some_object
+  {
+    ...;
+    byte[4] pre_here_marker_tag;        # == (g32 (iadd hereptr (ineg 8)))
+    byte[4] here_marker;
+    ... here_receiver;
+    ...;
+  };
+
+This is up to objects; callers don't need to know about tagging strategies
+because the here-marker itself isn't impacted.
